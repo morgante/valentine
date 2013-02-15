71abb13ca873e9a7f4116a7e691623629bbf0686
Morgante Pell <morgante.pell@morgante.net>
2013-02-15 18:00:19 +0400

    eh...

--- index.php
+++ index.php
@@ -1,163 +1,79 @@
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8">
-		<title>Crassus Investments, LLP</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="">
-		<meta name="author" content="">
-
-		<!-- Le styles -->
-		<link href="assets/css/bootstrap.min.css" rel="stylesheet">
-		<link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
-		<link href="assets/css/carousel.css" rel="stylesheet">
-		<link href="assets/css/style.css" rel="stylesheet">
-
-		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+		<link rel="stylesheet" type="text/css" href="style/style.css" /> 
+		<link rel="stylesheet" type="text/css" href="style/scrollpath.css" /> 
+		<link href="http://fonts.googleapis.com/css?family=Terminal+Dosis&subset=latin" rel="stylesheet" type="text/css">
+		<script type="text/javascript" src="script/lib/prefixfree.min.js"></script>
 		<!--[if lt IE 9]>
-			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
+		<meta name="description" content="The plugin that lets you define custom scroll paths" /> 
+		<title>jQuery Scroll Path</title>
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-25206568-1']);
+		  _gaq.push(['_trackPageview']);
 
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
 	</head>
-
-	<body data-spy="scroll" data-target=".navbar">
-
-		<!-- NAVBAR
-		================================================== -->
-		<div class="navbar-wrapper">
-			<!-- Wrap the .navbar in .container to center it within the absolutely positioned parent. -->
-			<div class="container">
-
-				<div class="navbar navbar-inverse navbar-fixed-to">
-					<div class="navbar-inner">
-						<!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
-						<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</a>
-						<a class="brand" href="#">Project name</a>
-						<!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
-						<div class="nav-collapse collapse">
-							<ul class="nav">
-								<li><a href="#values">Values</a></li>
-								<li><a href="#size">Size</a></li>
-								<li><a href="#global">Global</a></li>
-								<li><a href="#contact">Contact</a></li>
-								<!-- Read about Bootstrap dropdowns at http://twitter.github.com/bootstrap/javascript.html#dropdowns -->
-								<li class="dropdown" id="nav-partners">
-									<a class="dropdown-toggle" role="button" data-toggle="dropdown" data-target="#partners" href="#partners">Partners <b class="caret"></b></a> 
-									<ul class="dropdown-menu">
-										<li><a href="#morgante-pell">Morgante Pell</a></li>
-										<li><a href="#jim-bob">Jim Bob</a></li>
-										<li><a href="#smell-tab">Smell Tab</a></li>
-									</ul>
-								</li>
-							</ul>
-						</div><!--/.nav-collapse -->
-					</div><!-- /.navbar-inner -->
-				</div><!-- /.navbar -->
-
-			</div> <!-- /.container -->
-		</div><!-- /.navbar-wrapper -->
-
-
-
-
-		<!-- Marketing messaging and featurettes
-		================================================== -->
-		<!-- Wrap the rest of the page in another container to center all the content. -->
-
-		<div class="container marketing">
-		
-			<div class="hero-unit">
-				<p><strong>Crassus Investments</h1> is a <em>small</em> fund, with <em>global</em> partners, managed in a <em>socially-responsible</em> way.</p>
-		    </div>
-
-			<!-- START THE FEATURETTES -->
-
-			<hr class="featurette-divider">
-
-			<div class="featurette" id="values">
-				<img class="featurette-image pull-right" src="../assets/img/examples/browser-icon-chrome.png">
-				<h2 class="featurette-heading">Social Responsibility <span class="muted">is part of our culture</span></h2>
-				<p class="lead">At Crassus, we believe in ethical investing by carefully considering what ventures we choose to support. For us, this means actively <em>avoiding</em> companies which have a negative effect on society. We also <em>focus</em> investments on companies which we believe have the potential to improve the human condition.</p>
+	<body>
+		<nav>
+			<ul>
+				<li><a href="#start">1</a></li>
+				<li><a href="#description">2</a></li>
+				<li><a href="#syntax">3</a></li>
+				<li><a href="#scrollbar">4</a></li>
+				<li><a href="#rotations">5</a></li>
+				<li><a href="#rotations-rotated">6</a></li>
+				<li><a href="#source">7</a></li>
+				<li><a href="#follow">8</a></li>
+			</ul>
+		</nav>
+		<div class="setting">
+			<a href="" class="show-path">Show Path</a>
+		</div>
+		<div class="wrapper">
+			<div class="demo">
+				<h1>jQuery Scroll Path</h1>
+				<span class="arrow">&darr;</span> A Quick Demo <span class="arrow">&darr;</span>
 			</div>
-
-			<hr class="featurette-divider">
-
-			<div class="featurette" id="size">
-				<img class="featurette-image pull-left" src="../assets/img/examples/browser-icon-firefox.png">
-				<h2 class="featurette-heading">Small <span class="muted">but growing</span></h2>
-				<p class="lead">As a small fund, without large institutional investors, we are able to aggressively pursue opportunities which larger funds avoid due to risk or asset size. It is also a top priority to keep all our investors involved and educated about the choices we make as a fund.</p>
-			</div>
-
-			<hr class="featurette-divider">
-
-			<div class="featurette" id="global">
-				<img class="featurette-image pull-right" src="../assets/img/examples/browser-icon-safari.png">
-				<h2 class="featurette-heading">A global fund <span class="muted">in a digital age</span></h2>
-				<p class="lead">With <a href="#partners">partners</a> based around the world, Crassus is an inherently global fund. We have no physical office and conduct all business online. Here are some of the places our fund has touched:</p>
-				<div class="map">
-					MAP
-				</div>
-			</div>
-		
-			<hr class="featurette-divider">
-
-			<div class="featurette" id="contact">
-				<img class="featurette-image pull-right" src="../assets/img/examples/browser-icon-safari.png">
-				<h2 class="featurette-heading">Get in touch <span class="muted">with us</span></h2>
-				<p class="lead">If you are interested in finding out more about Crassus Investments, or investing in the fund, just email <a href="mailto:hello@crassusinvest.com">hello@crassusinvest.com</a> and we&apos;ll be in touch.</p>
-			</div>
-		
-			<hr class="featurette-divider">
-
-			<div class="featurette" id="partners">
-				<img class="featurette-image pull-right" src="../assets/img/examples/browser-icon-safari.png">
-				<h2 class="featurette-heading">Partners <span class="muted">from around the world</span></h2>
-				<p class="lead">The senior partners of Crassus Investments all studied together at <a href="http://uwc-usa.org">UWC-USA</a>.</p>
 			
-				<div class="partner" id="morgante-pell">
-					<img class="partner-image pull-right" src="../assets/img/examples/browser-icon-safari.png">
-					<h3 class="partner-name">Morgante Pell</h2>
-					<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				</div>
-				
-				<div class="partner" id="jim-bob">
-					<img class="partner-image pull-right" src="../assets/img/examples/browser-icon-safari.png">
-					<h3 class="partner-name">Morgante Pell</h2>
-					<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				</div>
-				
-				<div class="partner" id="smell-tab">
-					<img class="partner-image pull-right" src="../assets/img/examples/browser-icon-safari.png">
-					<h3 class="partner-name">Morgante Pell</h2>
-					<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				</div>
+			<div class="description">
+				<span class="big">It's a plugin for defining custom scroll paths.</span>
 			</div>
 
-			<hr class="featurette-divider">
+			<div class="syntax">
+				<span class="big">It uses canvas flavored syntax to draw lines and arcs.</span>
+			</div>
 
-			<!-- /END THE FEATURETTES -->
+			<div class="scrollbar">
+				<span class="big">It comes with a custom scrollbar.</span>
+			</div>
 
+			<div class="rotations">
+				<span class="big">It also does rotations</span>
+				<span class="upside-down big">in <a href="http://caniuse.com/#feat=transforms2d">supported</a> browsers.</span>
+			</div>
 
-			<!-- FOOTER -->
-			<footer>
-				<p class="pull-right"><a href="#">Back to top</a></p>
-				<p>&copy; 2012 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-			</footer>
+			<div class="source">
+				<span class="big">It's available with documentation on <a href="https://github.com/JoelBesada/scrollpath">GitHub</a>.<span>
+			</div>
 
-		</div><!-- /.container -->
-
-
-
-		<!-- Le javascript
-		================================================== -->
-		<!-- Placed at the end of the document so the pages load faster -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-		<script src="assets/js/bootstrap.min.js"></script>
-		<script src="assets/js/jquery.smooth-scroll.min.js"></script>
-		<script src="assets/js/main.js"></script>
+			<div class="follow">
+				<span class="big">Feel free to <a href="https://twitter.com/JoelBesada">follow me</a> on Twitter. You can also be <span class="count">a kind person and</span> <a href="https://twitter.com/intent/tweet?original_referer=https%3A%2F%2Ftwitter.com%2Fabout%2Fresources%2Fbuttons&source=tweetbutton&text=jQuery%20Scroll%20Path%20Plugin&url=http%3A%2F%2Fjoelb.me%2Fscrollpath%2F&via=JoelBesada" class="tweet">tweet</a> this.</span>
+			</div>
+		</div>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.pack.js"></script>
+	<script type="text/javascript" src="script/lib/jquery.easing.js"></script>
+	<script type="text/javascript" src="script/jquery.scrollpath.js"></script>
+	<script type="text/javascript" src="script/demo.js"></script>
 	</body>
 </html>
