50040b36a84c6720c54435de973d9c7a6c7bb6ce
Morgante Pell <morgante.pell@morgante.net>
2013-02-15 22:07:58 +0400

    super

--- index.php
+++ index.php
@@ -1,52 +1,101 @@
 
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
-		<link rel="stylesheet" type="text/css" href="assets/style.css" /> 
-		<link href="http://fonts.googleapis.com/css?family=Terminal+Dosis&subset=latin" rel="stylesheet" type="text/css">
-		<script type="text/javascript" src="script/lib/prefixfree.min.js"></script>
-		<!--[if lt IE 9]>
-		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
-		<meta name="description" content="The plugin that lets you define custom scroll paths" /> 
-		<title>jQuery Scroll Path</title>
-	</head>
-	<body>
-		<div class="wrapper">
-			<div class="start">
-				<h1>jQuery Scroll Path</h1>
-				<span class="arrow">&darr;</span> A Quick Demo <span class="arrow">&darr;</span>
-			</div>
-			
-			<div class="description">
-				<span class="big">It's a plugin for defining custom scroll paths.</span>
-			</div>
-
-			<div class="syntax">
-				<span class="big">It uses canvas flavored syntax to draw lines and arcs.</span>
-			</div>
-
-			<div class="scrollbar">
-				<span class="big">It comes with a custom scrollbar.</span>
-			</div>
-
-			<div class="rotations">
-				<span class="big">It also does rotations</span>
-				<span class="upside-down big">in <a href="http://caniuse.com/#feat=transforms2d">supported</a> browsers.</span>
-			</div>
-
-			<div class="source">
-				<span class="big">It's available with documentation on <a href="https://github.com/JoelBesada/scrollpath">GitHub</a>.<span>
-			</div>
-
-			<div class="follow">
-				<span class="big">Feel free to <a href="https://twitter.com/JoelBesada">follow me</a> on Twitter. You can also be <span class="count">a kind person and</span> <a href="https://twitter.com/intent/tweet?original_referer=https%3A%2F%2Ftwitter.com%2Fabout%2Fresources%2Fbuttons&source=tweetbutton&text=jQuery%20Scroll%20Path%20Plugin&url=http%3A%2F%2Fjoelb.me%2Fscrollpath%2F&via=JoelBesada" class="tweet">tweet</a> this.</span>
+<head>
+	<meta charset="utf-8"> 
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<title>SUPERSCROLLORAMA</title>
+	
+	<link href='http://fonts.googleapis.com/css?family=Luckiest+Guy' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="assets/normalize.css" type="text/css">
+	<link rel="stylesheet" href="assets/style.css" type="text/css">	
+</head>
+<body>
+	
+	<div id="background"></div>
+	
+	<div id="content-wrapper">
+		
+		<div id="start" class="section">
+			<div class="heart">
+				<h1 id="title"><strong>Dear</strong><span>Emma</em></h1>
+				<p>Many miles might separate us, but just</p>
+				<a class="continue" href="#unorthodox"><span class="arrow">↓</span>scroll with me<span class="arrow">↓</span></a>
 			</div>
 		</div>
-	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.pack.js"></script>
-	<script type="text/javascript" src="assets/jquery.easing.js"></script>
-	<script type="text/javascript" src="assets/jquery.scrollpath.js"></script>
-	<script type="text/javascript" src="assets/demo.js"></script>
-	</body>
-</html>
+		
+		<div id="unorthodox" class="section pinned">
+			<p>This might be a bit <strong>unorthodox</strong>, but so are we.</p>
+		</div>
+		
+		<div id="you" class="section pinned">
+			<p class="say">By that, I mean to say:</p>
+			<p class="wish">I couldn&apos;t wish for anyone</p>
+			
+			<div id="adjectives">
+				<h3 class="start">more</h2>
+				<ul>
+					<li>intelligent</li>
+					<li>generous</li>
+					<li>fantabulous</li>
+					<li>splendid</li>
+					<li>authentic</li>
+					<li>beautiful</li>
+					<li>sweet</li>
+					<li>amusing</li>
+					<li>entertaining</li>
+					<li>brilliant</li>
+					<li>sociable</li>
+					<li>enjoyable</li>
+					<li>pleasing</li>
+					<li>outgoing</li>
+					<li>happy</li>
+					<li>humorous</li>
+					<li>jolly</li>
+					<li>nice</li>
+					<li>witty</li>
+					<li>cute</li>
+					<li>friendly</li>
+					<li>joyful</li>
+					<li>radiant</li>
+					<li>kind</li>
+					<li>gracious</li>
+					<li>personable</li>
+					<li>engaging</li>
+					<li>lovable</li>
+					<li>loving</li>
+					<li>precious</li>
+					<li>giving</li>
+					<li>talented</li>
+					<li>lively</li>
+					<li>bright</li>
+					<li>trendy</li>
+					<li>intelligent</li>
+					<li>energetic</li>
+					<li>active</li>
+					<li>bold</li>
+					<li>understanding</li>
+					<li>brave</li>
+					<li>determined</li>
+					<li>responsible</li>
+					<li>wise</li>
+				</ul>
+				<h3 class="end">than you</h3>
+			</div>
+
+		</div>
+		
+		<div id="neruda" class="section pinned">
+			A bit of Neruda FOR YOU.
+		</div>
+		
+	</div>
+	
+	<script type="text/javascript" src="assets/tweenmax.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  	<script src="assets/jquery.smooth-scroll.js"></script>
+	<script src="assets/lettering.js"></script>
+	<script src="assets/superscrollorama.js"></script>
+	<script src="assets/main.js"></script>
+
+</body></html>
